0b94602904ca4c0f367030c7039c2b38c9d31861
Roritegg <ivan2006sky@gmail.com>
2025-12-06 18:32:43 +0300

    Update tic_tac_toe_widget.h

--- widget/tic_tac_toe_widget.h
+++ widget/tic_tac_toe_widget.h
@@ -4,6 +4,8 @@
 #include <QGridLayout>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QGroupBox>
+#include <QTableWidget>
 #include "D:\QDocs\TicTacToe\src\tic_tac_toe.h"
 
 class TicTacToeWidget : public QWidget {
@@ -15,18 +17,26 @@ public:
 private slots:
     void onCellClicked();
     void onResetClicked();
+    void onResetStatsClicked();
 
 private:
     void updateBoard();
     void updateStatus();
+    void updateStatsDisplay();
     void createGameBoard();
     void createControlPanel();
+    void createStatsPanel();
 
     TicTacToe game;
     QPushButton *cellButtons[3][3];
     QPushButton *resetButton;
+    QPushButton *resetStatsButton;
     QLabel *statusLabel;
     QLabel *currentPlayerLabel;
+
+    QLabel *statsLabel;
+    QTableWidget *statsTable;
+    QGroupBox *statsGroupBox;
 
     QWidget *gameBoardWidget;
     QWidget *controlPanelWidget;

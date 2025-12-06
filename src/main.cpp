50c1e98bbe1943b9026d1ac6a71a987d9a4eae88
Roritegg <ivan2006sky@gmail.com>
2025-12-06 18:31:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    app.setStyle("Fusion");
 
     TicTacToeWidget window;
     window.show();
 
     return app.exec();
 }
+

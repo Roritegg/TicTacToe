bf1c99c460b32b77ca50f25d7eba62ce4ab31e6f
Roritegg <ivan2006sky@gmail.com>
2025-12-08 23:30:58 +0300

    Upload widget

---
+++ src/tic_tac_toe_widget.h
@@ -0,0 +1,47 @@
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGroupBox>
+#include <QTableWidget>
+#include "tic_tac_toe.h"
+#include <QCheckBox>
+
+class TicTacToeWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit TicTacToeWidget(QWidget *parent = nullptr);
+
+private slots:
+    void onCellClicked();
+    void onResetClicked();
+    void onResetStatsClicked();
+
+private:
+    void updateBoard();
+    void updateStatus();
+    void updateStatsDisplay();
+    void createGameBoard();
+    void createControlPanel();
+    void createStatsPanel();
+
+    TicTacToe game;
+    QPushButton *cellButtons[3][3];
+    QPushButton *resetButton;
+    QPushButton *resetStatsButton;
+    QLabel *statusLabel;
+    QLabel *currentPlayerLabel;
+
+    QLabel *statsLabel;
+    QTableWidget *statsTable;
+    QGroupBox *statsGroupBox;
+
+    QWidget *gameBoardWidget;
+    QWidget *controlPanelWidget;
+    QVBoxLayout *mainLayout;
+
+    QCheckBox *soundCheckBox;
+};
